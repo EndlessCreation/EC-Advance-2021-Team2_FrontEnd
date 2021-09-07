9bec345980b383c8e8f388937f43afd53a34c00f
Jeongjin Oh <ojj991123@gmail.com>
2021-09-07 19:48:47 +0900

    Feat: post upload 구현

--- src/modules/index.jsx
+++ src/modules/index.jsx
@@ -2,15 +2,17 @@ import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import auth, { authSaga } from './auth';
 import loading from './loading';
+import post, { postSaga } from './post';
 import user, { userSaga } from './user';
 const rootReducer = combineReducers({
   auth,
   loading,
   user,
+  post,
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga()]);
+  yield all([authSaga(), userSaga(), postSaga()]);
 }
 
 export default rootReducer;
