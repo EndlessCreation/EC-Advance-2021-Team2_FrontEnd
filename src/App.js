2daa0bddfe5d7834bba506a3ec3567d56ca01dab
Jeongjin Oh <ojj991123@gmail.com>
2021-09-18 01:18:06 +0900

    Add findId and findPassword logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route } from 'react-router-dom';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import './App.css';
+import FindIdPage from './pages/FindIdPage';
+import FindPasswordPage from './pages/FindPasswordPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -19,6 +21,8 @@ const App = () => {
       <Route component={HomePage} path='/' exact />
       <Route component={LoginPage} path='/login' />
       <Route component={SignupPage} path='/signup' />
+      <Route component={FindIdPage} path='/findId' />
+      <Route component={FindPasswordPage} path='/findPassword' />
     </ThemeProvider>
   )
 }
---
+++ src/pages/FindPasswordPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FindPasswordContainer from '../containters/FindPasswordContainer';
+
+const FindPasswordPage = () => {
+    return (
+        <FindPasswordContainer />
+    );
+}
+
+export default FindPasswordPage;
